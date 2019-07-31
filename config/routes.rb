5957daf6ce0a0a18dc0628acda02d4ee7a083e8b
Abdul RonR <kdulsgm@gmail.com>
2019-07-31 20:59:30 +0300

    Initial rails Application

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+
+  #get 'pages/home', to: 'pages#home'
+  #get 'pages/about', to: 'pages#about'
+
+
+  root 'pages#home'
+  get 'about', to: 'pages#about' 
+
+  #resources :articles
+
+
+end
